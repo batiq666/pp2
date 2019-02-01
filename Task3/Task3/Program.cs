4994d92c16968565c60ad954de78be7fdfca5559
batiq666 <batiqboss@gmail.com>
2019-02-01 18:57:26 +0600

    eayaah

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine()); // размер массива 
+            int[] arr = new int[a];
+            string[] s = Console.ReadLine().Split(); // сохраняем числа в виде стринга 
+            for (int i = 0; i < arr.Length; i++) // фор
+            {
+                arr[i] = int.Parse(s[i]); // даю стрингам значение числа 
+                Console.Write(arr[i] + " " + arr[i] + " ");//выписываю числа два раза подряд 
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
