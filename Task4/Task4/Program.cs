0b7b618492fccb16fee931ed60e45f4c201e8fca
batiq666 <batiqboss@gmail.com>
2019-02-03 21:20:28 +0600

    asd123

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    if (i >= j)
+                    {
+                        Console.Write("[*]");
+
+                    }
+                    if (j == n)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
